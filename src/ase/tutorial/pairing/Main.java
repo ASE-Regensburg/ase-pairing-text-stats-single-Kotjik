fa463006052861057f0376366d15061120df0352
Konstantin <kulik.konstantin@gmx.de>
2019-11-29 13:26:46 +0100

    Versuch

--- src/ase/tutorial/pairing/Main.java
+++ src/ase/tutorial/pairing/Main.java
@@ -1,8 +1,68 @@
 package ase.tutorial.pairing;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("Text input: hahaha you fool");
+
+        System.out.println(countWordsUsingSplit("hahaha you fool"));
+        System.out.println(getAverageWordLength("hahaha you fool"));
+
+        //loesung
+
+    }
+
+    //Count Words
+    static int countWordsUsingSplit(String input) {
+        if (input == null || input.isEmpty()) {
+            return 0;
+        }
+
+        String[] words = input.split("\\s+");
+        return words.length;
+    }
+    //get average length of all words
+    static double getAverageWordLength(String input) {
+
+        String[] average = input.split(" ");
+
+        int count = 0;
+        int sum = 0;
+        int currentWordLength;
+        String tempString;
+        for(int i = 0; i < average.length; i++)
+        {
+            average[i] = average[i].replaceAll("[^a-zA-Z]", "").toLowerCase();
+            tempString = average[i];
+            currentWordLength = tempString.length();
+            sum += currentWordLength;
+            count++;
+        }
+        double averageWordLength = sum / count;
+        return averageWordLength;
+    }
+
+    static void countOccurencesOfEachWord(String input){
+        Map<String, String> map = new HashMap<String, String>();
+
+        if(input != null){
+            String[] separatedWords = input.split(" ");
+            for(String str: separatedWords){
+                if(map.containsKey(str)){
+                    int count = Integer.parseInt(map.get(str));
+                    map.put(str, String.valueOf(count + 1));
+                }else{
+                    map.put(str, "1");
+                }
+            }
+        }
+        //print map ...
+
     }
 }
+
+
